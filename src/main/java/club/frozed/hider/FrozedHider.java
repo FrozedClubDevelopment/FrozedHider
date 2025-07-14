5dfdf43e7a8cada46d4c3947f9c188dd39489d4d
Victor Abrego <elbitomc@gmail.com>
2025-07-14 16:39:09 -0500

    feat: created project, initial commit

---
+++ src/main/java/club/frozed/hider/FrozedHider.java
@@ -0,0 +1,20 @@
+package club.frozed.hider;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Elb1to
+ * @since 7/14/2025
+ */
+public final class FrozedHider extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
